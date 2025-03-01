141df737eca96a70d70ff6c2af692bbb9a8aeb8d
Filip Tomaśko <shader301270@gmail.com>
2025-03-01 00:36:17 +0000

    Add button to increment date

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,3 +1,4 @@
+use jiff::ToSpan;
 use leptos::prelude::*;
 use leptos_meta::{MetaTags, Stylesheet, Title, provide_meta_context};
 use leptos_router::{
@@ -55,7 +56,7 @@ fn HomePage() -> impl IntoView {
     view! {
       <h1 class="text-2xl font-bold text-red-400">"Welcome to Leptos!"</h1>
       <button
-        class="p-2 text-red-100 bg-blue-500 border-2 border-blue-600 shadow"
+        class="p-2 text-red-100 bg-blue-500 border-2 border-blue-600 shadow cursor-pointer"
         on:click=on_click
       >
         "Click Me: "
@@ -68,11 +69,29 @@ fn HomePage() -> impl IntoView {
 #[component]
 fn Post() -> impl IntoView {
     let now = jiff::Timestamp::now();
-    let date = jiff::tz::Offset::UTC.to_datetime(now);
+    let date = jiff::tz::db()
+        .get("Europe/Berlin")
+        .unwrap()
+        .to_datetime(now)
+        .with()
+        .subsec_nanosecond(0)
+        .build()
+        .unwrap();
+
+    let date = RwSignal::new(date);
+    let on_click = move |_| *date.write() += 1.day();
+
+    let display_date = move || format!("{} - {:#?}", date(), date().weekday());
 
     view! {
-      <article class="p-4 w-1/3 bg-amber-200">
-        <h3>{date.to_string()}</h3>
+      <article class="flex flex-col p-4 w-1/3 bg-amber-200">
+        <h3 class="self-start">{display_date}</h3>
+        <button
+          class="p-1 text-gray-400 bg-amber-300 cursor-pointer hover:bg-amber-400"
+          on:click=on_click
+        >
+          "Increase date"
+        </button>
         <p>
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a urna vel purus feugiat ultrices in in ipsum. Vestibulum sollicitudin pretium arcu, elementum ultrices erat sollicitudin ac. Morbi ornare lectus ut scelerisque porttitor. Curabitur faucibus nulla non ipsum ultricies interdum. Vestibulum dapibus enim ante, id ullamcorper ex placerat a. Vestibulum volutpat dui id dapibus aliquam. Sed facilisis ullamcorper mi eget fermentum."
         </p>
