ddf9d8be4ba6a456c2155e705a6b1b17b3faabbe
Filip Tomaśko <shader301270@gmail.com>
2025-03-05 16:34:04 +0000

    Update to axum 0.8

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,24 @@
 use app::*;
 use axum::Router;
+// use axum::extract::FromRef;
 use leptos::logging::log;
 use leptos::prelude::*;
 use leptos_axum::{LeptosRoutes, generate_route_list};
 
+// #[derive(FromRef, Debug, Clone)]
+// pub struct AppState {
+//     pub leptos_options: LeptosOptions,
+// }
+
 #[tokio::main]
-async fn main() {
-    let conf = get_configuration(None).unwrap();
+async fn main() -> anyhow::Result<()> {
+    let conf = get_configuration(None)?;
     let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
-    // Generate the list of routes in your Leptos App
+
     let routes = generate_route_list(App);
+
+    // let state = AppState { leptos_options };
 
     let app = Router::new()
         .leptos_routes(&leptos_options, routes, {
@@ -20,11 +28,9 @@ async fn main() {
         .fallback(leptos_axum::file_and_error_handler(shell))
         .with_state(leptos_options);
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     log!("listening on http://{}", &addr);
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
 }
